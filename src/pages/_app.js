c711d7ddf49422611d5a5974c996f491762e02c3
prinxard <prinxard@gmail.com>
2024-03-18 09:56:30 +0100

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <title>Ibeju Lekki Self Service Portal</title>
+      <title>KGIRS Local Government self portal</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
